fa2aec6d9eb4df908a311c33904bf8b7d737430a
haraldtv <harald.tverdal@protonmail.com>
2023-07-03 22:58:08 +0200

    Fixed intro and added github link to footer

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
     <!--<p style="max-width: 40em;">I'm an electronics engineering students with an interest in hardware and programming. This page contains some of my recent blog posts and projects :)</p> -->
     <img src="profile_pic.png" alt="Picture of me" style="width:15em;float:right">
 </p>
-<p style="max-width: 42em;">I'm an electronics engineering students with an interest in hardware and programming. This page contains some of my recent blog posts and projects. A lot of my original content on this site is free to use for any non-commercial purpouse, which you can read more about <a href="license.html">here</a>. Feel free to contact me at contact@tverdal.com :)</p>
+<p style="max-width: 42em;">I'm an electronics engineering students with an interest in hardware and software. This page contains some of my recent blog posts and projects. A lot of my original content on this site is free to use for any non-commercial purpouse, which you can read more about <a href="license.html">here</a>. Feel free to contact me at contact@tverdal.com :)</p>
 <?php
     echo "Dagens dato: ";
     echo date("d.m.y");
