a0eb1e69397fa0f32cb45563fc5559a167d2306d
haraldtv <harald.tverdal@protonmail.com>
2023-07-05 00:08:16 +0200

    Update admin.php

--- admin.php
+++ admin.php
@@ -5,9 +5,15 @@
     <div class="login_form">
         <form action="login.s.php" method="post">
 
+            <div class="uid_button">
             <input type="text" name="uid" placeholder="username">
+</div>
+<div class="pwd_button">
             <input type="password" name="pwd" placeholder="password">
-
+</div>
+<div class="login_button">
+            <button type="submit" name="submit">Login</button>
+</div>
         </form>
     </div>
 
