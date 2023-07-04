ac7fdb3a75d2991d69f9c110d35e095646935834
Harald Tverdal <harald@tverdal.com>
2023-07-04 22:48:46 +0200

    Added phptest

---
+++ phptest.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <title id="title">Harald Tverdal</title>
+</head>
+<body>
+
+    <?php 
+        require "header.html"
+        ?>
+
+    <div class="intro">
+<header><h1 class="title">PHP test</h1></header>
+<p>
+    <!--<p style="max-width: 40em;">I'm an electronics engineering students with an interest in hardware and programming. This page contains some of my recent blog posts and projects :)</p> -->
+    <img src="profile_pic.png" alt="Picture of me" style="width:15em;float:right">
+</p>
+<?php
+    $vari = 10
+    while ($vari >1) {
+        $vari--
+        echo "test";
+    }
+?>
+
+</div>
+
+<?php
+    require "footer.html"
+    ?>
+</body>
+</html>
