f0b253ebd0e0f36a50038f3378881065e4558c26
Harald Tverdal <harald@tverdal.com>
2023-07-04 23:01:18 +0200

    test

--- phptest.php
+++ phptest.php
@@ -14,11 +14,17 @@
 <header><h1 class="title">PHP test</h1></header>
 
 <?php
-    $vari = 0
-    while ($vari < 10) {
-        $vari += 1;
-        echo "test";
+    echo "<table border='1'>"
+    for ($ytre_teller=1; $ytre_teller<=9; $ytre_teller++) {
+        echo "<tr>";
+
+        for ($indre=1; $indre<=9; $i++) {
+            $tall = $indre*$ytre_teller;
+            echo "<td>$tall</td>";
+        }
+        echo "</tr>";
     }
+    echo "</table>";
 ?>
 
 <p>Hello World</p>
