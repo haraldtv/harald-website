ab9d58c8942689e29a850c6278a021e2a610851a
Harald Tverdal <harald@tverdal.com>
2023-07-04 23:04:20 +0200

    test

--- phptest.php
+++ phptest.php
@@ -4,7 +4,7 @@
     for ($ytre_teller=1; $ytre_teller<=9; $ytre_teller++) {
         echo "<tr>";
 
-        for ($indre=1; $indre<=9; $i++) {
+        for ($indre=1; $indre<=9; $indre++) {
             $tall = $indre*$ytre_teller;
             echo "<td>$tall</td>";
         }
