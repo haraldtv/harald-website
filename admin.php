73deb99e2c5afb4c756caebb20c5743e038b1126
haraldtv <harald.tverdal@protonmail.com>
2023-07-05 00:01:10 +0200

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,14 @@
+<section class="login_page">
+
+    <header><h1 class="title">Admin login</h1></header>
+
+    <div class="login_form">
+        <form action="login.s.php" method="post">
+
+            <input type="text" name="uid" placeholder="username">
+            <input type="password" name="pwd" placeholder="password">
+
+        </form>
+    </div>
+
+</section>
