1c74e4ff129bba42984721bcaed89fb484d71d78
Harald Tverdal <harald@tverdal.com>
2023-07-04 23:01:51 +0200

    test

--- phptest.php
+++ phptest.php
@@ -1,17 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-    
-    <title id="title">Harald Tverdal</title>
-</head>
-<body>
-
-    <?php 
-        require "header.html"
-        ?>
-
-    <div class="intro">
-<header><h1 class="title">PHP test</h1></header>
 
 <?php
     echo "<table border='1'>"
@@ -26,13 +12,3 @@
     }
     echo "</table>";
 ?>
-
-<p>Hello World</p>
-
-</div>
-
-<?php
-    require "footer.html"
-    ?>
-</body>
-</html>
