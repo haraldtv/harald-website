7277be26448a29e5a750b5ac6613b17bb147afb3
Harald Tverdal <harald@tverdal.com>
2023-07-04 22:44:00 +0200

    test

--- projects/internet-speed-testing/project_internet-speed-testing.php
+++ projects/internet-speed-testing/project_internet-speed-testing.php
@@ -12,7 +12,7 @@
     <p><h1>Internet speed testing</h1></p>
     <p> I recently needed to check if there was an issue with my new router, or if the connection to our ISP was the problem. 
         I then wrote this script to run continiously on a computer connected to my router by cable. 
-        THe script runs a speed test at set intervals and logs the results to a textfile.
+        The script runs a speed test at set intervals and logs the results to a textfile.
     </p>
     <p><a href="https://github.com/haraldtv/internet-test">Link</a> to github page</p>
 
