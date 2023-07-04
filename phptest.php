28b5ea8c526ae04d880627e0d14ef45013cd6e32
Harald Tverdal <harald@tverdal.com>
2023-07-04 22:53:35 +0200

    test

--- phptest.php
+++ phptest.php
@@ -11,15 +11,10 @@
 
     <div class="intro">
 <header><h1 class="title">PHP test</h1></header>
-<p>
-    <!--<p style="max-width: 40em;">I'm an electronics engineering students with an interest in hardware and programming. This page contains some of my recent blog posts and projects :)</p> -->
-    <img src="profile_pic.png" alt="Picture of me" style="width:15em;float:right">
-</p>
+
 <?php
-    $vari = 10
-    while ($vari >1) {
-        $vari--
-        echo "test";
+    for ($i=0; $i<10; $i++) {
+        echo "Test";
     }
 ?>
 
