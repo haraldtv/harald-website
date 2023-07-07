2c0b51e5a3dc23c1db0f69b5cf4b399234e274ad
Harald Tverdal <harald@tverdal.com>
2023-07-07 15:05:07 +0200

    test

--- admin/includes/admin.inc.php
+++ admin/includes/admin.inc.php
@@ -3,6 +3,7 @@
     define ("USERNAME", "admin");
     if (($_REQUEST["pwd"] == PASSWORD_KEY) and ($_REQUEST["uid"]) == USERNAME) {
         echo "SUCESS!!";
+        echo $_SERVER['REMOTE_ADDR'];
     }
     else 
         echo "Failure";
