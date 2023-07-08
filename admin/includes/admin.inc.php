53e18f831cf56fab1da87a78f36a60c76506c35f
Harald Tverdal <harald@tverdal.com>
2023-07-08 10:40:57 +0200

    test

--- admin/includes/admin.inc.php
+++ admin/includes/admin.inc.php
@@ -2,6 +2,9 @@
     define ("PASSWORD_KEY", "passord");
     define ("USERNAME", "admin");
     if ((($_REQUEST["pwd"] == PASSWORD_KEY) and ($_REQUEST["uid"])  ) == USERNAME) {
+        
+        echo "<link rel='stylesheet' href='/style.css'>";
+
         echo "SUCESS!!";
         echo "<br>Your ip: $_SERVER[REMOTE_ADDR]";
 
