305a709d91f5da600dd3a8ae3224ca8265176436
Harald Tverdal <harald@tverdal.com>
2023-07-09 13:47:08 +0200

    test

--- admin/index.php
+++ admin/index.php
@@ -1,9 +1,29 @@
+<?php 
+    if ((isstet($_POST[pwd]) and ($_POST[pwd] == 'passord') {
+        define ("PASSWORD_KEY", "passord");
+        define ("USERNAME", "admin");
+        if ((($_REQUEST["pwd"] == PASSWORD_KEY) and ($_REQUEST["uid"])  ) == USERNAME) {
+
+            echo "SUCESS!!";
+            echo "<br>Your ip: $_SERVER[REMOTE_ADDR]";
+
+            echo "<br";
+            echo "<header><h1>Admin panel</h1></header>";
+
+            echo "<form ";
+        }
+        else 
+            echo "Failure";
+}
+else {
+?>
+
 <section class="login_page">
 
     <header><h1 class="title">Admin login</h1></header>
 
     <div class="login_form">
-        <form action="includes/admin.inc.php" method="post">
+        <form action="" method="post">
 
             <div class="uid_button">
                 <input type="text" name="uid" placeholder="username">
@@ -18,3 +38,7 @@
     </div>
 
 </section>
+
+<?php 
+    }
+?>
