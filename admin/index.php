3f5d62a0ea688a2eba3e8e404f28826165fab3d1
haraldtv <harald.tverdal@protonmail.com>
2023-07-10 18:17:32 +0200

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -1,13 +1,11 @@
 <html>
 <?php 
-define ("PASSWORD_KEY", "passord");
+define ("PASSWORD_KEY", file_get_contents('../../pswd/adminpass'));
 define ("USERNAME", "admin");
 
     if ( (isset($_POST["pwd"])) and ($_POST["pwd"] == PASSWORD_KEY) ) {
     
         if ((($_REQUEST["pwd"] == PASSWORD_KEY) and ($_REQUEST["uid"])  ) == USERNAME) {
-
-            echo file_get_contents('../../pswd/adminpass');
 
             echo "SUCESS!!";
             echo "<br>Your ip: $_SERVER[REMOTE_ADDR]";
