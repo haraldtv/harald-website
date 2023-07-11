98674391d019ffc93a2434b58842a2e49db21565
haraldtv <harald.tverdal@protonmail.com>
2023-07-11 14:55:30 +0200

    Update phptest.php

--- phptest.php
+++ phptest.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-if (isset($_SESSION["teller"])) {
-    $_SESSION["teller"]++;
+if (isset($_COOKIE"teller"])) {
+    $_COOKIE["teller"]++;
 }
 else
-    $_SESSION["teller"] = 1;
+    setcookie("teller", 1, time()+3600);
     ?>
     <p> Antall besøk: <?php echo $_SESSION["tellet"] ?> </p>
