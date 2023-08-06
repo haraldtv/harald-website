3b04bff75c5626c2b16b8fb7ba66736a5c69d9da
Harald Tverdal <harald@tverdal.com>
2023-08-06 23:10:25 +0200

    test

--- blog/index.php
+++ blog/index.php
@@ -10,7 +10,7 @@
         require "../header.html"
         ?>
 
-<p>This is where i would put my blog posts.....if i had any!</p>
+<p>This is where future blog posts will go</p>
 <p><a href="blogPosts/2023/2023_06_03-Test_post/2023_06_03-Test-post.html">Test post</a> - June 3rd 2023</p>
 
 
