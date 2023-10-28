f2cf6f19ef74a1b297df25a541b62ce66ad3cc2f
haraldtv <129341220+haraldtv@users.noreply.github.com>
2023-10-28 17:33:41 +0200

    Update index.php

--- projects/index.php
+++ projects/index.php
@@ -14,7 +14,6 @@
 <p><a href="./notebooks/pi_calculating.html">Notebook on calculating pi</a></p>
 <p><a href="https://github.com/haraldtv/MEK2000">My notes on MEK2000 (maths 2000)</a></p>
 <p><a href="https://github.com/haraldtv/DAVE3700">My notes on DAVE3000 (maths 3000)</a></p>
-<p><a href="">Password showcase</a></p>
 <p><a href="https://tverdal.com/projects/internet-speed-testing/project_internet-speed-testing.php">Internet speed testing</a></p>
 
 
