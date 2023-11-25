3e87c7c82eb488c167dfe96e71a9e449ed63133e
haraldtv <129341220+haraldtv@users.noreply.github.com>
2023-11-25 23:41:48 +0100

    Fixed spelling

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
     <div class="intro">
 <header><h1 class="title">Harald Martin Tverdal</h1></header>
 <p>
-    <!--<p style="max-width: 40em;">I'm an electronics engineering students with an interest in hardware and programming. This page contains some of my recent blog posts and projects :)</p> -->
+    <!--<p style="max-width: 40em;">I'm an electronics engineering student with an interest in hardware and programming. This page contains some of my recent blog posts and projects :)</p> -->
     <img src="profile_pic.png" alt="Picture of me" style="width:15em;float:right">
 </p>
 <p style="max-width: 42em;">I'm an electronics engineering students with an interest in hardware and software. This page contains some of my recent blog posts and projects. Feel free to contact me at contact@tverdal.com :)</p>
