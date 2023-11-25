f85bcd4ab3b994d565b5dcfe5702ab7071158678
haraldtv <129341220+haraldtv@users.noreply.github.com>
2023-11-25 23:47:03 +0100

    fixed the correct spelling

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
     <!--<p style="max-width: 40em;">I'm an electronics engineering student with an interest in hardware and programming. This page contains some of my recent blog posts and projects :)</p> -->
     <img src="profile_pic.png" alt="Picture of me" style="width:15em;float:right">
 </p>
-<p style="max-width: 42em;">I'm an electronics engineering students with an interest in hardware and software. This page contains some of my recent blog posts and projects. Feel free to contact me at contact@tverdal.com :)</p>
+<p style="max-width: 42em;">I'm an electronics engineering student with an interest in hardware and software. This page contains some of my recent blog posts and projects. Feel free to contact me at contact@tverdal.com :)</p>
 
 
 </div>
